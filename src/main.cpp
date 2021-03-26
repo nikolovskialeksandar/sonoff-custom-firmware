7359d6f031765242d1b3dd95a72f2ecf5ccfd5b2
segasunset95 <nikolovskialexxandar@gmail.com>
2021-03-26 01:46:43 +0100

    Change server, set up WebSocket client

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,51 @@
-#include <FirebaseArduino.h>
 #include <ESP8266WiFi.h>
+#include <WebSocketsClient.h>
 #include <config.h>
 
 const char WIFI_SSID[] =  SSID;
 const char WIFI_PASSWORD[]  = PASSWORD;
-const char FIREBASE_HOST[]  = FIREBASE_PROJECT_ID;
-const char FIREBASE_AUTH[]  = FIREBASE_DATABASE_SECRET;
+const char URL[] = SERVER_URL;
+const int PORT = SERVER_PORT;
+
+WebSocketsClient webSocket;
 
 int LED_PIN = 13;
 int RELAY_PIN = 12;
 int BTN_PIN = 0;
 
-void setup() {
-  Serial.begin(115200);
+void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
+	switch(type) {
+		case WStype_DISCONNECTED:
+			Serial.printf("[WSc] Disconnected!\n");
+			break;
+		case WStype_CONNECTED: 
+      Serial.printf("[WSc] Connected to url: %s\n", payload);
+			break;
+		case WStype_TEXT:
+      // Change state when WebSocket message is received (using led pin for testing purposes)
+      String message = (char*)payload;
+      if (message == String("true")) {
+        digitalWrite(LED_PIN, LOW);
+      } else if (message == String("false")) {
+        digitalWrite(LED_PIN, HIGH);
+      }
+      break;
+  }
+}
+
+void setup() {  
+	Serial.begin(115200);
+
+  // Set pins, initalize relay state
   pinMode(LED_PIN, OUTPUT);
   pinMode(RELAY_PIN, OUTPUT);
-  pinMode(BTN_PIN, OUTPUT);
+  pinMode(BTN_PIN, INPUT);
   digitalWrite(RELAY_PIN, LOW);
 
+  // Connect to WiFi
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.print("Connecting to Wi-Fi");
-  while (WiFi.status() != WL_CONNECTED)
-  {
+  while (WiFi.status() != WL_CONNECTED) {
     Serial.print(".");
     delay(300);
   }
@@ -30,18 +54,11 @@ void setup() {
   Serial.println(WiFi.localIP());
   Serial.println();
 
-  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  // Connect to WebSocket server, handle WebSocket events
+  webSocket.begin(URL, PORT);
+  webSocket.onEvent(webSocketEvent);
 }
 
 void loop() {
-  bool relayState = Firebase.getBool("sonoff/sonoffSwitch");
-  Serial.println(relayState);
-  if(relayState == true) {
-    digitalWrite(RELAY_PIN, HIGH);
-    digitalWrite(LED_PIN, HIGH);
-  }
-  else {
-    digitalWrite(RELAY_PIN, LOW);
-    digitalWrite(LED_PIN, LOW);
-  }
+  webSocket.loop();
 }
